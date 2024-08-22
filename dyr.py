5e2fdf67d56ee8cfb03dd344e4c75ae882a4cf7f
Jon-slow97 <lok7@hi.is>
2024-08-22 17:32:39 +0000

    Bætti við kottur í dyr.py

--- dyr.py
+++ dyr.py
@@ -2,8 +2,13 @@ import sys
 
 def default():
     print('halló hæ')
+def kottur():
+    print('Mjá Mjá')
 def main():
-    default()
+    if sys.argv[1]== 'kottur':
+        kottur()
+    else:
+        default()
 
 if __name__ == '__main__':
     main()
